a48ac35d8c1d4490afd2a0c38f3b0586a0ce14c6
odaialazzeh <odai.alazzeh@gmail.com>
2022-12-06 17:14:32 +0200

    Update Screen.js

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Screen = () => (
-  <div className="screen-row">
-    <input type="text" value={0} readOnly />
-  </div>
-);
+class Screen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return <div className="screen-row"><input type="text" value={0} readOnly /></div>;
+  }
+}
 export default Screen;
