87f22c9b2f2bac39243fe2e1c4b0cb5b733a702b
odai alazzeh <odai.alazzeh@gmail.com>
2022-12-21 15:05:21 +0200

    Update calculator.test.js

--- src/Test/calculator.test.js
+++ src/Test/calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Calculator from '../components/Calculator';
+import Calculator from '../components/Calculator.js';
 
 describe('Calculator', () => {
   test('summation correctly', () => {
